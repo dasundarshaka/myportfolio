af51ff20f9ada5448ad096e610fb0d9a6b5f4d2b
dasundarshaka <dasun.darshaka01@gmail.com>
2025-11-05 17:22:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/myport/", // ensures relative paths for Netlify
+  base: "/myportfolio/", // ensures relative paths for Netlify
 })
